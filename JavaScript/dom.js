19fbe07cb81174919fe736f953cb0912c5722013
Shikhyank <deepshikakumari72@gmail.com>
2023-03-16 23:43:30 +0530

    Dom7

--- JavaScript/dom.js
+++ JavaScript/dom.js
@@ -1,4 +1,4 @@
-//EXAMINE THE DOCEMENT OBJECT//
+// EXAMINE THE DOCEMENT OBJECT//
 
 // console.dir(document)
 // console.log(document.domain)
@@ -19,14 +19,68 @@
 //     li[i].style.backgroundColor='#f4f4f4';
 // }
 
-var item= document.querySelector('.list-group-item:nth-child(2)');
-item.style.color='green';
+// var item= document.querySelector('.list-group-item:nth-child(2)');
+// item.style.color='green';
 
 // var titles= document.querySelectorAll('.title');
 // console.log(titles);
 // titles[0].textContent='Hello';
-var odd=document.querySelectorAll('li:nth-child(odd)');
+// var odd=document.querySelectorAll('li:nth-child(odd)');
 
-for(var i=0;i<odd.length;i++){
-    odd[i].style.backgroundColor='green';
-}
+// for(var i=0;i<odd.length;i++){
+//     odd[i].style.backgroundColor='green';
+// }
+//PARENTNODE
+// var itemlist=document.querySelector('#items')
+// console.log(itemlist.parentNode)
+// itemlist.parentNode.style.backgroundColor='gray'
+// console.log(itemlist.parentNode.parentNode.parentNode)
+
+//PARENTELEMENT
+var itemlist=document.querySelector('#items')
+// console.log(itemlist.parentElement)
+// itemlist.parentElement.style.backgroundColor='gray'
+// console.log(itemlist.parentElement.parentElement.parentElement)
+
+//childNode
+// console.log(itemlist.childNodes)
+
+//children
+
+// console.log(itemlist.children)
+// itemlist.children[1].style.backgroundColor='yellow'
+
+// console.log(itemlist.firstElementChild)
+// itemlist.firstElementChild.textContent='Hello 1'
+
+// console.log(itemlist.nextSibling)
+// console.log(itemlist.nextElementSibling)
+
+//create element
+
+//create div
+
+var newDiv=document.createElement('div')
+
+// console.log(newDiv)
+newDiv.className='Hello';
+
+newDiv.id='Hello1'
+newDiv.setAttribute('title','Hello div')
+
+//add text content
+
+var newDivText=document.createTextNode('Hello World')
+
+newDiv.appendChild(newDivText);
+
+
+var container=document.querySelector('header.container');
+var h1=document.querySelector('header h1');
+console.log(newDiv);
+newDiv.style.fontSize='30px';
+container.insertBefore(h1);
+// container.insertAdjacentElement(h1,newDiv)
+
+
+
