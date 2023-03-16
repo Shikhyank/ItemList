6dd659add79137c6bfd053cfcef1428ad55e9106
Shikhyank <deepshikakumari72@gmail.com>
2023-03-16 12:03:26 +0530

    DOM5

--- JavaScript/dom.js
+++ JavaScript/dom.js
@@ -7,14 +7,14 @@
 // document.title=123
 // console.log(document.all)
 
-var items= document.getElementsByClassName('list-group-item');
-console.log(items)
-console.log(items[1])
-items[1].textContent='Hello 2';
-items[1].style.fontWeight='bold';
-items[1].style.backgroundColor='yellow';
+var items= document.getElementsByTagName('li');
+console.log(li)
+console.log(li[1])
+li[1].textContent='Hello 2';
+li[1].style.fontWeight='bold';
+li[1].style.backgroundColor='yellow';
 
-for(var i=0;i<items.length;i++){
+for(var i=0;i<li.length;i++){
 
-    items[i].style.backgroundColor='#f4f4f4';
+    li[i].style.backgroundColor='#f4f4f4';
 }
